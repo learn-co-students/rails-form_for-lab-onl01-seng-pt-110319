b6c3efc4c285369f8ffe34ca4be4b9491773a737
nicksolony <nicksolony@gmail.com>
2020-04-12 16:42:28 -0400

    fixed migration

--- db/migrate/20200412203950_create_school_classes.rb
+++ db/migrate/20200412203950_create_school_classes.rb
@@ -1,7 +1,7 @@
 class CreateSchoolClasses < ActiveRecord::Migration[5.0]
   def change
     create_table :school_classes do |t|
-      t.string, :title
+      t.string :title
       t.integer :room_number
 
       t.timestamps
