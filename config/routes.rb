1125c851f0db3ff8453ae12c6a852ee4f4e2c3e0
nicksolony <nicksolony@gmail.com>
2020-04-12 16:41:05 -0400

    creted controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'school_classes/new'
+
+  post 'school_classes/create'
+
+  get 'school_classes/show'
+
+  get 'school_classes/edit'
+
+  patch 'school_classes/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
