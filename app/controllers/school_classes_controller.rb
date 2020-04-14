d9643705ab6fbbec6429a2aecd7fdf5a62876110
nicksolony <nicksolony@gmail.com>
2020-04-13 23:56:13 -0400

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,5 +1,6 @@
 class SchoolClassesController < ApplicationController
   def new
+    @school_class=SchoolClass.new
   end
 
   def create
@@ -9,12 +10,17 @@ class SchoolClassesController < ApplicationController
   end
 
   def show
+    @school_class=SchoolClass.find(params[:id])
   end
 
   def edit
+    @school_class=SchoolClass.find(params[:id])
   end
 
   def update
+    @school_class=SchoolClass.find(params[:id])
+    @school_class.update(post_params(:school_class,[:title, :room_number]))
+    redirect_to school_class_path(@school_class)
   end
 
 end
